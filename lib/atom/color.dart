01611b55dbd88c1e1ebaaa8c44d56722898d4bba
velia-vito <91265151+velia-vito@users.noreply.github.com>
2024-02-07 12:27:00 +0530

    Construct a new underlying Color implementation.
    Signed-off-by: velia-vito <91265151+velia-vito@users.noreply.github.com>

--- lib/atom/color.dart
+++ lib/atom/color.dart
@@ -6,7 +6,11 @@ import 'dart:ui' show Color;
 
 // Package imports:
 import 'package:material_color_utilities/hct/hct.dart';
+import 'package:material_color_utilities/hct/viewing_conditions.dart';
 
 // Sub-Parts.
 part 'color/hct_utils.dart';
+part 'color/color_role.dart';
+part 'color/m_hct.dart';
+
 part 'color/color_palette.dart';
